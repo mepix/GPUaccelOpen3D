5b46f40db951dc2cce610860b863aac4d83c24b0
mepix <13284555+mepix@users.noreply.github.com>
2021-11-19 12:05:05 -0800

    Added Colors & Create Combined NumPy Array

--- scripts/libopen3d.py
+++ scripts/libopen3d.py
@@ -4,7 +4,7 @@ import numpy as np
 import open3d as o3d
 
 class WrapperOpen3d(object):
-    """docstring for ."""
+    """This lightweight wrapper on Open3D converts a .ply point cloud into a NumPy Array"""
 
     def __init__(self, path_to_ply):
         self.path_to_ply = path_to_ply
@@ -31,47 +31,75 @@ class WrapperOpen3d(object):
             print("No Point Cloud Loaded, Cannot Visualize")
         return None
 
-    def getNumPyData(self):
+    def getNumPyPts(self):
         """Returns the opened point cloud as a NumPy Array"""
         if self.point_cloud_loaded:
             return np.asarray(self.pcd.points)
         else:
-            print("No Point Cloud Loaded, Cannot Get NumPy Array")
+            print("No Point Cloud Loaded, Cannot Get Points")
             return None
 
-    def getNormals(self,asNumPy=True):
+    def getNumPyNorms(self):
         """Estimates the normals for reach point, returns as NumPy Array if True"""
         # self.pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
         if self.point_cloud_loaded:
-            if asNumPy:
-                return np.asarray(self.pcd.normals)
-            else:
-                return self.pcd.normals
+            return np.asarray(self.pcd.normals)
         else:
             print("No Point Cloud Loaded, Cannot Get Normals")
             return None
 
-    def test(self):
-        # Open the Point Cloud
-        self.plyOpen()
+    def getNumPyColors(self):
+        """Returns the opened point colors as a NumPy Array"""
+        if self.point_cloud_loaded:
+            return np.asarray(self.pcd.colors)
+        else:
+            print("No Point Cloud Loaded, Cannot Get Colors")
+            return None
 
-        # Visualize the Point Cloud
-        self.visPointCloud()
+    def getNumpyAll(self,verbose=False):
+        """Returns an array of [X,Y,Z,R,G,B,NormX,NormY,NormZ]"""
+        # Get a Points
+        npd = self.getNumPyPts()
+        if verbose:
+            print(npd.shape)
+            print(npd)
 
-        # Get a NumPy Array
-        npd = self.getNumPyData()
-        print(npd)
+        # Get Colors
+        npc = self.getNumPyColors()
+        if verbose:
+            print(npc.shape)
+            print(npc)
 
         # Get Normals
-        norms = self.getNormals(True)
-        print(norms)
+        npn = self.getNumPyNorms()
+        if verbose:
+            print(npn.shape)
+            print(npn)
 
-        return None
+        # Concatenate Array
+        npa = np.hstack((npd,npc,npn))
+        if verbose:
+            print(npa.shape)
+            print(npa)
+
+        # Output combined NumPy array
+        return npa
 
 if __name__ == '__main__':
     try:
         print("Testing Open3D")
+
+        # Intialize the Class
         myOpen3d = WrapperOpen3d("../data/LeafPointCloud.ply")
-        myOpen3d.test()
+
+        # Open the Point Cloud
+        myOpen3d.plyOpen()
+
+        # Visualize the Point Cloud
+        myOpen3d.visPointCloud()
+
+        # Get the NumPy Data (Pts,Colors,Normals)
+        myNumPyArray = myOpen3d.getNumpyAll(verbose=True)
+
     except:
         print("ERROR, EXCEPTION THROWN")
